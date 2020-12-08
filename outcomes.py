3fa3594facf7cfa9c246be612b32884bae04f08f
ruot-nyak <nyak.g.ruot@gmail.com>
2020-12-08 14:29:02 -0800

    finialized outcomes.py

--- outcomes.py
+++ outcomes.py
@@ -1,32 +1,46 @@
 import random
-import Player 
+from player import Player  
 
 class Outcomes(Player):
-    def __init__(self,):
-        Player.__init__(self,'Ruot',100)
-        
-    def choice(self,player):
+    def __init__(self, name, current_health):
+        super().__init__(name, current_health)
+
+    
+    def choice(self):
         odds = [1,2,3,4,5,6]
         choice_good = f'Hmm {self.name} you are very lucky n/ No death!'
-        if (random.choice(odds) == 1):
+        option = random.choice(odds)
+
+        if (option == 1):
             self.current_health -= 100
             return f'sorry {self.name}, you fell in a well and took some damage n/ hp: {self.current_health}'
-        elif (random.choice(odds) == 2):
+        elif (option == 2):
             self.current_health -= 100
             return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
-        elif (random.choice(odds) == 3):
+        elif (option == 3):
             self.current_health -= 100
             return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
-        elif (random.choice(odds) == 4):
+        elif (option == 4):
             self.current_health -= 100
             return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
-        elif (random.choice(odds) == 5):
+        elif (option == 5):
             self.current_health -= 100
             return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
         else:
             return choice_good
 
+class RightLeft(Outcomes):
+    # weather or not the user chooses right or left
+    def __init__(self, name, current_health):
+        super().__init__(name, current_health)
 
-    
+    def move(self):
+        movement = input('You see two tunnels left or right?')
+        if (movement == 'right' or movement == 'left'):
+            return self.choice()
 
-
+if __name__ == "__main__":
+    outcomes = Outcomes('Ruot',100)
+    outcomes.choice()
+    test_move = RightLeft('Ruot',100)
+    test_move.move()
--- player.py
+++ player.py
@@ -1,37 +1,34 @@
 
+
 # Creating the players class which includes name and starting health
-class player:
-    def __init__(self, name, starting_health= 0):
+class Player:
+    def __init__(self, name, current_health=0):
         self.name = name
-        self.starting_health = int(starting_health)
-        self.current_health = int(starting_health)
+        self.current_health = int(current_health)
+        # prints for debugging purposes
         print(f'{self.name},{self.current_health}')
-#Starting the game
-    # def player_name(self):
-    #     self.name = str(input('I haven\'t seen you around here /n are you a what is your name?')
-    #     return
-    #
+# Starting the game
 
-#     def start_adventure(self):
-#         start_game = str(input('Welcome to Death Dungeon. Would you like to enter? y or n?'))
-#         if (start_game == 'y' or start_game == 'yes'):
-#             return f'Welcome to Death Dungeon {self.name} n/ I will be guiding you to your death.'
-#         else:
-#             print('What are you scared!?')
-#             return quit
+    def player_name(self):
+        # self.name = str(input('I haven\'t seen you around here /n are you a what is your name?')
+        pass
 
-#     def player_class(self):
-#         pass
-# class test:
-#     pass
+    def start_adventure(self):
+        start_game = str(
+            input('Welcome to Death Dungeon. Would you like to enter? y or n?'))
+        if (start_game == 'y' or start_game == 'yes'):
+            return f'Welcome to Death Dungeon {self.name} n/ I will be guiding you to your death.'
+        else:
+            print('What are you scared!?')
+            return quit
 
+    def player_class(self):
+        pass
 
 
 
 
-
-    
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    test_person = player('Ruot', 100)
+    test_person = Player('Ruot', 100)

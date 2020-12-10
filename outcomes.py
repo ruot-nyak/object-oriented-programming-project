9ea203f8e4aec5112b937c1a0898c40345450032
ruot-nyak <nyak.g.ruot@gmail.com>
2020-12-10 15:51:59 -0800

    final commit

--- outcomes.py
+++ outcomes.py
@@ -2,45 +2,89 @@ import random
 from player import Player  
 
 class Outcomes(Player):
-    def __init__(self, name, current_health):
+    #defined these variables outside of a function so they can be called to any function
+    #with in the class
+    odds = [1,2,3,4,5,6]
+    option = random.choice(odds)
+
+    def __init__(self, name, current_health,damage):
         super().__init__(name, current_health)
+        self.damage = damage
+
 
     
     def choice(self):
-        odds = [1,2,3,4,5,6]
         choice_good = f'Hmm {self.name} you are very lucky n/ No death!'
-        option = random.choice(odds)
+        self.damage = '.Your Damage : -100'
 
-        if (option == 1):
+#IF statement to grab on of the options defined above the class, to then return an outcome
+        if (self.option == 1):
+            #Taking -100 to current health integer 100
             self.current_health -= 100
-            return f'sorry {self.name}, you fell in a well and took some damage n/ hp: {self.current_health}'
-        elif (option == 2):
+            #return a message that returns the users name, as well as their current health after the damgage taken
+            #then returning damage.
+            return f'Haha {self.name}, clumsy, you fell in a well. n/ hp: {self.current_health} {self.damage}'
+        elif (self.option == 2):
             self.current_health -= 100
-            return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
-        elif (option == 3):
+            return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health} {self.damage}'
+        elif (self.option == 3):
             self.current_health -= 100
-            return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
-        elif (option == 4):
+            return f'Sorry {self.name}, you had a leg cramp n/ hp: {self.current_health} {self.damage}'
+        elif (self.option == 4):
             self.current_health -= 100
-            return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
-        elif (option == 5):
+            return f'Sorry {self.name}, you had a bear in a headlock, tripped and hit your head n/ hp: {self.current_health} {self.damage}'
+        elif (self.option == 5):
             self.current_health -= 100
-            return f'Sorry {self.name}, you hurt your foot n/ hp: {self.current_health}'
+            return f'Sorry {self.name}, you forgot to wear a bandaid, and got a cut n/ hp: {self.current_health} {self.damage}'
         else:
             return choice_good
 
-class RightLeft(Outcomes):
-    # weather or not the user chooses right or left
-    def __init__(self, name, current_health):
-        super().__init__(name, current_health)
 
+class Movement(Outcomes):
+ 
+    death = print(f'invalid response, now you have to die....')
+    # weather or not the user chooses right or left
+    def __init__(self, name, current_health,damage,msg):
+        super().__init__(name, current_health,damage)
+        self.msg = msg
+        self.right_left = input('You see two tunnels left or right?')
     def move(self):
-        movement = input('You see two tunnels left or right?')
-        if (movement == 'right' or movement == 'left'):
-            return self.choice()
+        self.msg = 'You take a step down the tunnel and ...'
+        if (self.right_left == 'right' or self.right_left == 'left'):
+            return print(f'{self.msg} {self.choice()}')
+        else:
+            self.current_health -= 1000
+            return print(f'{self.death()} {self.damage()}')
+
+
+class End(Player):
+    message_list = [
+    'HAHAHA YOU ARE DEAD GOODBYE!',
+    'Unlucky, or staged. You\'ll never know. ',
+    'We\'re not built the same I guess.',
+    'LMAO that\'s how you died?',]
+    message = print(random.choice(message_list))
+    def __init__(self, name, current_health):
+        super().__init__(name, current_health,)
+        
+    
+
+    def game_over(self):
+        player_death = self.current_health <= 0
+        if (player_death == True):
+            return self.message
+        else:
+            pass
 
 if __name__ == "__main__":
-    outcomes = Outcomes('Ruot',100)
-    outcomes.choice()
-    test_move = RightLeft('Ruot',100)
+    # start_game =    
+    # start_game = str(
+    #     input('Welcome to Death Dungeon. Would you like to enter? y or n?')) 
+    test_person = Player('Ruot', 100)
+    test_person.start_adventure()
+
+    # outcomes = Outcomes('Ruot',100,0)
+    # outcomes.choice()
+    test_move = Movement('Ruot',100,0,0)
     test_move.move()
+    
--- player.py
+++ player.py
@@ -1,34 +1,32 @@
-
+import random
 
 # Creating the players class which includes name and starting health
+
+
 class Player:
+
     def __init__(self, name, current_health=0):
         self.name = name
         self.current_health = int(current_health)
+        self.start_game = str(
+        input('Welcome to Death Dungeon. Would you like to enter? y or n?'))
+        self.loop_game = 'You must be scared.'
         # prints for debugging purposes
         print(f'{self.name},{self.current_health}')
 # Starting the game
 
-    def player_name(self):
-        # self.name = str(input('I haven\'t seen you around here /n are you a what is your name?')
-        pass
-
     def start_adventure(self):
-        start_game = str(
-            input('Welcome to Death Dungeon. Would you like to enter? y or n?'))
-        if (start_game == 'y' or start_game == 'yes'):
-            return f'Welcome to Death Dungeon {self.name} n/ I will be guiding you to your death.'
-        else:
-            print('What are you scared!?')
-            return quit
-
-    def player_class(self):
-        pass
-
-
-
+        if (self.start_game == 'y' or self.start_game == 'yes'):
+            print(f'Welcome to Death Dungeon {self.name} I will be guiding you to your death. \n {self.loop_game}')
+        elif (self.start_game == 'n' or self.start_game =='no'):
+            self.loop_game
+            return self.start_game
+next
 
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
+    start_game = str(
+        input('Welcome to Death Dungeon. Would you like to enter? y or n?'))
     test_person = Player('Ruot', 100)
+    test_person.start_adventure()
